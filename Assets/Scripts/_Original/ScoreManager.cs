19c28776e3328a11a29549ea989643ff99a257f0
danish <danishalif16@gmail.com>
2023-10-28 18:12:52 +0700

    game over music

--- Assets/Scripts/_Original/ScoreManager.cs
+++ Assets/Scripts/_Original/ScoreManager.cs
@@ -7,8 +7,11 @@ public class ScoreManager : MonoBehaviour
 {
     public GameManager2 gameManager;
     [SerializeField] TextMeshProUGUI timeScoreText, ConnectedScoreText, totalScoreText;
+    [SerializeField] AudioSource gameBGM, gameoverBGM;
 
     private void Awake() {
+        gameBGM.Stop();
+        gameoverBGM.Play();
         timeScoreText.text = gameManager.timeScore.ToString();
         ConnectedScoreText.text = gameManager.connectedScore.ToString();
         totalScoreText.text = gameManager.totalScore.ToString();
